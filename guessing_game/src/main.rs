78d68ae546ac855da048751ed0e5e3e4196131df
Peter Duan <zddua@student.unimelb.edu.au>
2024-05-20 16:16:16 +1000

    giaogiao

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Hi WWB 你好汪文博！");
-    println!("这是我基于Rust 语言构建的脚本");
+    println!("Hi Hello World!");
+    println!("欢迎来到我的快乐猜数游戏");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("读取失败");
+    let secret_num = rand::thread_rng().gen_range(1..=100);
     
-    println!("you type: {guess}");
+    loop {
+        let mut guess = String::new();
+        println!("请猜测一个数字：");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("读取失败");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你的猜测 : {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
     
 }
